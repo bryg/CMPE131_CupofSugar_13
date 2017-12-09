2b4c9e91d544ddeac0f456baff5d8732f30b77a8
Bryan Gallardo <bryanjgallardo@gmail.com>
2017-12-09 01:07:47 -0800

    Fixed inconsistencies.
    Changed 'ratings' to 'sugarcubes'.

--- login.sql
+++ login.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
-<<<<<<< HEAD
--- Generation Time: Dec 06, 2017 at 12:27 AM
-=======
 -- Generation Time: Dec 06, 2017 at 07:39 AM
->>>>>>> refs/remotes/origin/master
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -85,11 +81,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-<<<<<<< HEAD
-INSERT INTO `users` (`id`, `username`, `rating`, `password`, `email`, `homeaddr`, `stateid`, `sugartype`, `profilepic`, `cellphone`, `zipcode`) VALUES
-=======
+
 INSERT INTO `users` (`id`, `username`, `sugarcubes`, `password`, `email`, `homeaddr`, `stateid`, `sugartype`, `profilepic`, `cellphone`, `zipcode`) VALUES
->>>>>>> refs/remotes/origin/master
 (1, 'divyang', 0, 'password123', NULL, '', '', 0, 0, 0, 0),
 (10, 'Bob Newbie', 0, 'Password123', 'bnewbie@hmail.com', '', '', 0, 0, 0, 0),
 (11, 'Panfilo', 0, 'qwerty', 'panfilo@mail.com', '', '', 0, 0, 0, 0),
